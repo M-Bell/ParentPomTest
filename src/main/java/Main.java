4abd2cf5139277d096a8623f910dab7dd5ba7ff0
Maksym Shyiko <maxymshyjko@gmail.com>
2022-01-17 14:14:43 +0200

    Test for maven dependency

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,9 @@
+import org.springframework.beans.BeanMetadataAttribute;
 
 public class Main {
     public static void main(String[] args) {
-
+        //Spring connected from pom
+        System.out.println(BeanMetadataAttribute.class.getCanonicalName());
     }
 }
 
